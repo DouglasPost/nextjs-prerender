7f1f4891829860730904e79ad0b0317b387f5211
Douglas Post <88281271+DouglasPost@users.noreply.github.com>
2022-05-30 14:57:20 -0600

    Added server and client side fetching. Dummy project finished

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
-import path from 'path';
-import fs from 'fs/promises';
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
 
 function HomePage(props) {
   const { products } = props;
   return (
     <ul>
-      {products.map(product => (<li key={product.id}>{product.title}</li>))}
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
     </ul>
   );
 }
 
 export async function getStaticProps(context) {
-  console.log('(Re-)Generating');
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  console.log("(Re-)Generating");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
-  return { props: {
-    products: data.products,
+  return {
+    props: {
+      products: data.products,
     },
     revalidate: 10,
   };
